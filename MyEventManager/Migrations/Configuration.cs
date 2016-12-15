3971e957d3d53cb7e8ca0e558fa56fbc1b1c1569
Brandon Clarke <clarkeb94@hotmail.com>
2016-12-15 00:57:40 -0500

    EventManagerCommit

---
+++ MyEventManager/Migrations/Configuration.cs
@@ -0,0 +1,38 @@
+namespace MyEventManager.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyEventManager.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyEventManager.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.Roles.AddOrUpdate(m => m.Name, new IdentityRole { Name = "User" });
+            context.Roles.AddOrUpdate(m => m.Name, new IdentityRole { Name = "Artist" });
+
+           
+        }
+    }
+}
